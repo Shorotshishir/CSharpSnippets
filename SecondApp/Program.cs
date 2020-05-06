7a308ffd8c6b7f1554a8a9f095f2db4997a17274
Siratim Mustaquim <siratim@pop-os.localdomain>
2020-05-07 00:58:05 +0900

    update 1

--- SecondApp/Program.cs
+++ SecondApp/Program.cs
@@ -8,15 +8,23 @@ namespace SecondApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Non-Static Class
             var myclass = new MyClass();
             myclass.Age = 10;
             myclass.Home = "Earth";
-            var type = typeof(Environment); // MyClass is static class with static properties
-            foreach (var p in type.GetProperties())
+            System.Console.WriteLine("Printing attribute data from MyClass");
+            foreach (var p in typeof(MyClass).GetProperties())
+            {
+                System.Console.WriteLine($"{p.Name}: {p.GetValue(myclass,null)}");
+            }
+
+            System.Console.WriteLine("Showing Your pc's information using Environment Class's attributes");
+             // var type = typeof(Environment); // Environment is static class with static properties
+            // Static Class
+            foreach (var p in typeof(Environment).GetProperties())
             {
                 System.Console.WriteLine($"{p.Name}: {p.GetValue(null)}");
             }
-            System.Console.WriteLine();
         }
     }
 }
---
+++ SecondApp/readme.md
@@ -0,0 +1,3 @@
+# Cycling Through a class's attributes
+
+This code snippets shows how to loop through all the attributes of a class , print out the name of the attributes and the value that they are holding
